d1b4c9b1b9572c5ea413e8a6b7ed53dd59b6ab98
liubinyi <binyiliu@BinYis-MacBook-Pro.local>
2014-10-23 00:37:38 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module('breakroom', ['ngRoute'])
+	.config(breakroomRouter);
+function breakroomRouter($routeProvider) {
+	$routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html'
+        })
+        .when('/about', {
+          templateUrl: 'partials/about.html'
+        })
+        .when('/services', {
+          templateUrl: 'partials/services.html'
+        })
+        .when('/portfolio', {
+          templateUrl: 'partials/portfolio.html'
+        })
+        .when('/blog', {
+          templateUrl: 'partials/blog.html'
+        })
+        .when('/contact', {
+          templateUrl: 'partials/contact.html'
+        })
+}
+function navController($scope, $location) {
+	$scope.isActive = function (viewLocation) {
+		return viewLocation === $location.path();
+	};
+}
